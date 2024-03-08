c375848162e95f565b8fecf5f198c0ffbeea1419
IkanAsinn <qbin.hermawan@gmail.com>
2024-03-08 22:07:14 +0700

    test PcapWriter

--- sniffer.py
+++ sniffer.py
@@ -1,4 +1,5 @@
-from scapy.all import sniff, wrpcap
+from scapy.all import sniff
+from scapy.utils import PcapWriter
 from datetime import datetime
 from time import sleep
 from os import path
@@ -8,6 +9,7 @@ class Sniffer:
     def __init__(self, sniffing_active: Event, shutdown_signal: Event):
         self.sniffing_active = sniffing_active
         self.shutdown_signal = shutdown_signal
+        self.pkt_dump = PcapWriter('captured_packets.pcap', append=True, sync=True)
 
     def start_sniffing(self):
         self.sniffing_active.set()
@@ -22,17 +24,16 @@ class Sniffer:
         with open('temporary_packets.log', 'a') as temp_log:
             temp_log.write(f"{datetime.now()} - {packet.summary()}\n")
 
+        self.pkt_dump.write(packet)
+
     def sniff_packets(self):
         while not self.shutdown_signal.is_set() and self.sniffing_active.is_set():
             if not path.exists('temporary_packets.log'):
                 with open('temporary_packets.log', 'w'):
                     pass
 
-            packets = sniff(timeout=5, prn=self.packet_callback, store=0)
+            sniff(timeout=5, prn=self.packet_callback, store=0)
             
-            # Save captured packets to a PCAP file
-            wrpcap('captured_packets.pcap', packets)
-
             with open('temporary_packets.log', 'r') as temp_log:
                 logs = temp_log.read()
 
